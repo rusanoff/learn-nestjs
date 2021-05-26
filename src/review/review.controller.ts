e2ebf0ce896eee416f327c47f178f2e579ae8820
Andrey Rusanov <andrew.rusanoff@gmil.com>
2021-05-26 04:32:27 +0300

    Review module service methods

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,20 +1,34 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { ReviewService } from './review.service';
+import { REVIEW_NOT_FOUND } from './review.constants';
 import { ReviewModel } from './review.model';
 
 @Controller('review')
 export class ReviewController {
-  @Post('create')
-  async create(@Body() dto: Omit<ReviewModel, '_id'>) {
+  constructor(private readonly reviewService: ReviewService) {}
 
+  @Post('create')
+  async create(@Body() dto: CreateReviewDto) {
+    return this.reviewService.create(dto);
   }
 
   @Delete(':id')
   async delete(@Param('id') id: string) {
+    const deletedDoc = await this.reviewService.delete(id);
 
+    if (!deletedDoc) {
+      throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
+    }
   }
 
   @Get('byProduct/:productId')
   async getByProduct(@Param('productId') productId: string) {
+    return this.reviewService.findByProductId(productId);
+  }
 
+  @Delete(':id')
+  async deleteByProductId(@Param('id') id: string) {
+    return this.reviewService.deleteByProductId(id);
   }
 }
